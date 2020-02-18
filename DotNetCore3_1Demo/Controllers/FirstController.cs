96afa0d290bbcb6491ced403da3135ea2295c0b5
iwangfeng7 <1220654545@qq.com>
2020-02-18 16:37:54 +0800

    add page send value by session viewData viewBag TempData

---
+++ DotNetCore3_1Demo/Controllers/FirstController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DotNetCore3_1Demo.Controllers
+{
+    public class FirstController : Controller
+    {
+        private readonly ILogger<FirstController> _logger;
+        private readonly ILoggerFactory _loggerFactory;
+        public FirstController(ILogger<FirstController> logger,
+            ILoggerFactory loggerFactory)
+        {
+            _logger = logger;
+            this._loggerFactory = loggerFactory;
+        }
+
+        public IActionResult Index()
+        {
+            this._logger.LogWarning("This is FirstController-Index");
+            this._loggerFactory.CreateLogger<FirstController>().LogWarning("This is FirstController-Index 1");
+
+            base.ViewBag.User1 = "ViewBag_iwangfeng7";
+            base.ViewData["User2"] = "ViewData_iwangfeng7";
+            base.TempData["User3"] = "TempData_iwangfeng7";
+            //session需要配置
+            string result = base.HttpContext.Session.GetString("User4");
+            if (string.IsNullOrWhiteSpace(result))
+            {
+                base.HttpContext.Session.SetString("User4", "Session_iwangfeng7");
+            }
+
+            object name = "Model_iwangfeng7";
+
+            return View(name);
+        }
+    }
+}
